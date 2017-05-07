19603d73927282ca6ca3388f5ba7cb9ce7d05f77
CPC67128 <steve.fuchs@free.fr>
2017-05-07 23:47:04 +0200

    Fix few issues

--- v2/contact_files_detail.php
+++ v2/contact_files_detail.php
@@ -21,7 +21,7 @@ for ($i = 0; $i < $n; $i++)
 ?>
 	<img class="imageView" src="download.php?file_id=<?php echo $rowFile["file_id"]; ?>" border=0 /><br />
 	<a href="download.php?file_id=<?php echo $rowFile["file_id"]; ?>" target="blank"><?php echo $rowFile["original_filename"]; ?></a> (<?php echo $rowFile["creation_date"]; ?>)
-	<button data-dojo-type="dijit.form.Button" onclick="var confirmation = confirm('Supprimer ?'); if (confirmation) { dojo.xhrPost({url: '../prm_controllers/file_controller.php?type=remove_file&file_id=<?php echo $rowFile["file_id"]; ?>', load: function(data, ioArgs) { RefreshLeftPanel(); RefreshCenterPanel(); }}); }">Supprimer</button>
+	<button data-dojo-type="dijit.form.Button" onclick="DeleteContactFile(<?= $rowFile["file_id"] ?>);">Supprimer</button>
 <?php
 		if ($row["picture_file_id"] != $rowFile["file_id"])
 		{
@@ -34,22 +34,21 @@ for ($i = 0; $i < $n; $i++)
 	{
 ?>
 	<a href="download.php?file_id=<?php echo $rowFile["file_id"]; ?>" target="blank"><?php echo $rowFile["original_filename"]; ?></a> (<?php echo $rowFile["creation_date"]; ?>)
-	<button data-dojo-type="dijit.form.Button" onclick="var confirmation = confirm('Supprimer ?'); if (confirmation) { dojo.xhrPost({url: '../prm_controllers/file_controller.php?type=remove_file&file_id=<?php echo $rowFile["file_id"]; ?>', load: function(data, ioArgs) { RefreshCenterPanel(); }}); }">Supprimer</button>
+	<button data-dojo-type="dijit.form.Button" onclick="DeleteContactFile(<?= $rowFile["file_id"] ?>);">Supprimer</button>
 <?php
 	}
 }
 ?>
 
 <script type="text/javascript" charset="utf-8">
-function DeleteContactAttribute(contactId, attributeId) {
-	console.log("DeleteContactAttribute() called");
-
+function DeleteContactFile(fileId) {
+	console.log("DeleteContactFile() called");
 	var confirmation = confirm('Confirmer ?');
 	if (confirmation) {
-		$.post('../prm_controllers/contact_controller.php?type=remove_attribute&contact_id='+contactId+'&contact_attribute_id='+attributeId,
+		$.post('../prm_controllers/file_controller.php?type=remove_file&file_id='+fileId,
 			{ },
 			function(data, status){
-				LoadContactAttributesDetail();
+				LoadContactFilesDetail();
 			});
 	}
 }
