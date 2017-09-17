367dfc5d5c7770cd9d6225142bf4e9491de9abe4
CPC67128 <steve.fuchs@free.fr>
2017-09-17 14:57:57 +0200

    Upgrade to BootStrap v4.0.0-beta

--- views/index.php
+++ views/index.php
@@ -35,7 +35,7 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-toggleable-sm navbar-inverse fixed-top bg-inverse">
+  	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <button class="navbar-toggler navbar-toggler-right hidden-md-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
